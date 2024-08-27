94a0f52742c1656f4a46f62541bf1b4ed0bc731d
김태우 <t.wooo@kakao.com>
2024-08-27 19:38:03 +0900

    [#3] - feat: 로그인 후 메인 페이지로 이동하도록 설정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,16 @@
+import { useAuth } from 'app';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LoginContainer, LoginFields, Page } from 'widgets';
 
 const LoginPage: React.FC = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  if (user) {
+    navigate('/', { replace: true });
+  }
+
   return (
     <Page height="100vh">
       <LoginContainer>
