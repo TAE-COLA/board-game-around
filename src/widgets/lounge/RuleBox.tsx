3e14a39c02a2180ad276d670fdbf32c303eec684
김태우 <t.wooo@kakao.com>
2024-09-02 23:48:28 +0900

    [#5] - feat: RuleBox 수정

--- src/widgets/lounge/RuleBox.tsx
+++ src/widgets/lounge/RuleBox.tsx
@@ -1,4 +1,4 @@
-import { Flex, FlexProps } from '@chakra-ui/react';
+import { Flex, FlexProps, Text } from '@chakra-ui/react';
 import { Game } from 'entities';
 import React from 'react';
 
@@ -11,8 +11,9 @@ const RuleBox: React.FC<IProps> = ({
   ...props
 }) => {
   return (
-    <Flex direction='column' paddingX='6' paddingY='4' background='gray.100' borderRadius='md' {...props}>
-      게임명: {game.name}
+    <Flex direction='column' paddingX='6' paddingY='4' gap='4' background='gray.100' borderRadius='md' {...props}>
+      <Text fontSize='xl' fontWeight='bold'>{game.name} 게임이 곧 시작됩니다!</Text>
+      <Text>게임 설명: {game.description}</Text>
     </Flex>
   )
 };
