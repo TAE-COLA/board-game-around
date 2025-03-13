9813d8807fed1ebb8ac5afe2c42170e141510ca8
김태우 <t.wooo@kakao.com>
2025-03-13 12:50:00 +0900

    [#8] - prettier

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <Providers>
